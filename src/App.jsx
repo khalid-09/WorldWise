2cf8eab328a1f8b95fd2611de9e18dfbcac0476d
Khalid Ansari <khaliljpc@gmail.com>
2023-11-15 21:35:13 +0530

    Pages/Components and Routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Product from './pages/Product';
 import Homepage from './pages/Homepage';
+import Product from './pages/Product';
 import Pricing from './pages/Pricing';
-import PageNotFound from './pages/PageNotFound';
+import Login from './pages/Login';
 import AppLayout from './pages/AppLayout';
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
   return (
     <div>
-      <h1>Hello Router</h1>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
+          <Route path="login" element={<Login />} />
           <Route path="app" element={<AppLayout />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
