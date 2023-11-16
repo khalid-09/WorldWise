9c298ccc676f39e674cc3e5678b90dee9cffc2d7
Khalid Ansari <khaliljpc@gmail.com>
2023-11-16 23:14:22 +0530

    Added City and Countries nested routes and Implemented Parameters and QueryStrings for URL

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,25 @@
+import { useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
 
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
+  return (
+    <div className={styles.mapContainer}>
+      <h1>Map</h1>
+      <p>
+        Position : {lat} : {lng}
+      </p>
+      <button
+        onClick={() => {
+          setSearchParams({ lat: 23, lng: 45 });
+        }}
+      >
+        Change pos
+      </button>
+    </div>
+  );
 }
 
 export default Map;
